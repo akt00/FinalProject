e0f0b0746c5d0bee88f01dcabb0222e768c880f5
Aki <msf5pbsi@gmail.com>
2024-02-04 19:41:54 +0900

    implemented focal losses

--- losses.py
+++ losses.py
@@ -13,10 +13,10 @@ class DiceLoss(nn.Module):
         kwargs:
             smoothing (float): avoids division by zero. default is 1.0
     """
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, smooth=1.) -> None:
         super(DiceLoss, self).__init__()
         # smoothing value to avoid division by zero
-        self.smoothing = kwargs.get('smoothing', 1.0)
+        self.smooth = smooth
 
     def forward(self, preds, targets):
         """ returns the mean dice loss
@@ -33,8 +33,7 @@ class DiceLoss(nn.Module):
         intersection = (_preds * _targets).sum()
         # approximation of union for computational efficiency
         appx_union = _preds.sum() + _targets.sum()
-        dice_coeff = (intersection + self.smoothing) / (appx_union
-                                                        + self.smoothing)
+        dice_coeff = (intersection + self.smooth) / (appx_union + self.smooth)
         # dice_loss = 1 - dice_coeff
         return 1 - dice_coeff
 # the cell ends here
@@ -43,10 +42,10 @@ class DiceLoss(nn.Module):
 class FocalLoss(nn.Module):
     mode = 'binary'
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.gamma = kwargs.get('gamma', 2.0)
-        self.alpha = kwargs.get('alpha', 0.25)
+    def __init__(self, alpha=.25, gamma=2.) -> None:
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
 
     def forward(self, preds, targets):
         _preds = preds.view(-1)
@@ -77,6 +76,38 @@ class FocalDiceLoss(nn.Module):
         return self.focal_loss(preds, targets) + self.dice_loss(preds, targets)
 
 
+class TverskyLoss(nn.Module):
+    def __init__(self, alpha=0.7, smooth=1.) -> None:
+        super().__init__()
+        self.alpha = alpha
+        self.smooth = smooth
+
+    def tversky(self, preds, targets):
+        _preds = preds[:, 0].contiguous().view(-1)
+        _targets = targets[:, 0].contiguous().view(-1)
+        tp = (_preds * _targets).sum()
+        fn = (_targets * (1 - _preds)).sum()
+        fp = ((1 - _targets) * _preds).sum()
+        return (tp + self.smooth) / (
+            tp + self.alpha * fn + (1 - self.alpha) * fp + self.smooth)
+
+    def forward(self, preds, targets):
+        return 1 - self.tversky(preds, targets)
+
+
+class FocalTverskyLoss(nn.Module):
+    def __init__(self, alpha=.7, gamma=.75, smooth=1.) -> None:
+        super().__init__()
+        self.alpha = alpha
+        self.smooth = smooth
+        self.tversky = TverskyLoss(alpha=self.alpha, smooth=self.smooth)
+        self.gamma = gamma
+
+    def forward(self, preds, targets):
+        tversky_score = self.tversky(preds, targets)
+        return (1 - tversky_score).pow(self.gamma)
+
+
 # the code in this section is originally written by Akihiro Tanimoto
 class BCEDiceLoss(nn.Module):
     """ Binary cross entropy loss with dice loss
@@ -105,10 +136,11 @@ class BCEDiceLoss(nn.Module):
 # the code in this section is originally written by Akihiro Tanimoto
 if __name__ == '__main__':
     import torch
-    # tests the losses
-    loss_fn = BCEDiceLoss()
-    in_shape = [32, 1, 256, 256]
-    preds = torch.randn(*in_shape).clamp(min=0, max=1)
-    gt = (torch.randn(*in_shape) > .5).float()
-    print(loss_fn(preds, gt))
+
+    t1 = torch.randn(8, 1, 10, 10)
+    t1 = (t1 > 0.5).float()
+    t2 = torch.randn(8, 1, 10, 10)
+    t2 = (t2 > 0.5).float()
+    loss_fn = FocalTverskyLoss()
+    print(loss_fn(t1, t2))
 # the cell ends here
